9e350bf7ace168fe9500511f0e4ef0ba829d4dec
Kraigus <eliseev.anton96@yandex.ru>
2023-02-05 20:14:03 +0300

    add Sidebar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,7 +2,6 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './Navbar.module.scss';
 import { AppLink } from 'shared/ui/AppLink/AppLink';
 import { AppLinkTheme } from 'shared/ui/AppLink/AppLink';
-import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 interface NavbarProps {
     className?: string;
 }
@@ -10,7 +9,6 @@ interface NavbarProps {
 export const Navbar = ({ className }: NavbarProps) => {
     return (
         <div className={classNames(cls.navbar, {}, [className])}>
-            <ThemeSwitcher className={''}/>
             <div className={cls.links}>
                 <AppLink
                     theme={AppLinkTheme.SECONDARY}
