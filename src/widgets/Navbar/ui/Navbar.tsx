f2d9bc0949c1b8da999150adad54909edf564ef2
Kraigus <eliseev.anton96@yandex.ru>
2023-02-05 18:46:39 +0300

    add routeConfig, Navbar, ui component AppLink

---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,27 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './Navbar.module.scss';
+import { AppLink } from 'shared/ui/AppLink/AppLink';
+import { AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+
+interface NavbarProps {
+    className?: string;
+}
+
+export const Navbar = ({ className }: NavbarProps) => {
+    return (
+        <div className={classNames(cls.navbar, {}, [className])}>
+            <div className={cls.links}>
+                <AppLink
+                    theme={AppLinkTheme.SECONDARY}
+                    to={'/'}
+                    className={cls.mainLink}
+                >
+                    Главная
+                </AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>
+                    О сайте
+                </AppLink>
+            </div>
+        </div>
+    );
+};
