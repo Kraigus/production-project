b5e0bdde685634064981b9a9d10a94bd7c07494c
Anton <eliseev.anton96@yandex.ru>
2024-01-20 23:22:54 +0300

    fixed some storybook issues

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -12,13 +12,9 @@ export default ({ config }: { config: webpack.Configuration }) => {
         locales: '',
         buildLocales: '',
     };
-    config.resolve!.alias = { '@': paths.src };
     config!.resolve!.modules!.push(paths.src);
     config!.resolve!.extensions!.push('.ts', '.tsx');
-    config!.resolve!.modules = [
-        path.resolve(__dirname, '../../src'),
-        'node_modules',
-    ];
+    config.resolve!.alias = { ...config!.resolve!.alias, '@': paths.src };
     // eslint-disable-next-line no-param-reassign
     // @ts-ignore
     config!.module!.rules = config.module!.rules!.map((rule: RuleSetRule) => {
