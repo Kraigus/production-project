4eb48c6df8909a13407e9c7abdfbbfb96c1acd30
Anton <eliseev.anton96@yandex.ru>
2024-01-25 16:14:35 +0300

    add helpers for actions, selectors, slices

--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -1,27 +1,29 @@
-import { useDispatch, useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { Button } from '@/shared/ui/Button';
-import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
-import { counterActions } from '../model/slice/counterSlice';
+import { useCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
+import { useCounterActions } from '../model/slice/counterSlice';
 
 export const Counter = () => {
-    const dispatch = useDispatch();
-    const counterValue = useSelector(getCounterValue);
-    const increment = () => {
-        dispatch(counterActions.increment());
-    };
-
-    const decrement = () => {
-        dispatch(counterActions.decrement());
-    };
+    const counterValue = useCounterValue();
     const { t } = useTranslation();
+    const { increment, decrement, add } = useCounterActions();
+    const handleInc = () => {
+        increment();
+    };
+    const handleDec = () => {
+        decrement();
+    };
+    const handleAddFive = () => {
+        add(5);
+    };
     return (
         <div>
             <h1 data-testid="value-title">
                 {counterValue}
             </h1>
-            <Button data-testid="increment-btn" onClick={increment}>{t('Increment')}</Button>
-            <Button data-testid="decrement-btn" onClick={decrement}>{t('Decrement')}</Button>
+            <Button data-testid="increment-btn" onClick={handleInc}>{t('Increment')}</Button>
+            <Button data-testid="decrement-btn" onClick={handleDec}>{t('Decrement')}</Button>
+            <Button data-testid="add-btn" onClick={handleAddFive}>{t('add five')}</Button>
         </div>
     );
 };
