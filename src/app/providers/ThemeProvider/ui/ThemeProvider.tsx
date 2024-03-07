58ba82db03e4dcb04bd0829eb16fd53b56e8b79c
Anton <eliseev.anton96@yandex.ru>
2024-03-07 17:44:45 +0300

    add jsonSettings, add init auth data from server

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,10 +1,7 @@
-import { useState, useMemo, ReactNode } from 'react';
+import { ReactNode, useEffect, useMemo, useState } from 'react';
 import { ThemeContext } from '../../../../shared/lib/context/ThemeContext';
 import { Theme } from '@/shared/const/theme';
-import { LOCAL_STORAGE_THEME_KEY } from '@/shared/const/localStorage';
-
-const defaultTheme =
-    (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.LIGHT;
+import { useJsonSettings } from '@/entities/User';
 
 interface ThemeProviderProps {
     initialTheme?: Theme;
@@ -12,8 +9,21 @@ interface ThemeProviderProps {
 }
 
 const ThemeProvider = (props: ThemeProviderProps) => {
-    const { children, initialTheme } = props;
-    const [theme, setTheme] = useState<Theme>(initialTheme || defaultTheme);
+    const { initialTheme, children } = props;
+    const { theme: defaultTheme } = useJsonSettings();
+    const [isThemeInited, setThemeInited] = useState(false);
+
+    const [theme, setTheme] = useState<Theme>(
+        initialTheme || defaultTheme || Theme.LIGHT,
+    );
+
+    useEffect(() => {
+        if (!isThemeInited && defaultTheme) {
+            setTheme(defaultTheme);
+            setThemeInited(true);
+        }
+    }, [defaultTheme, isThemeInited]);
+
     const defaultProps = useMemo(
         () => ({
             theme,
--- src/features/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/features/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,9 +1,11 @@
-import { memo } from 'react';
+import { memo, useCallback } from 'react';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { useTheme } from '@/shared/lib/hooks/useTheme/useTheme';
 import ThemeSwitcherIcon from '@/shared/assets/icons/theme-switcher.svg';
 import { Button, ThemeButton } from '@/shared/ui/Button';
 import cls from './ThemeSwitcher.module.scss';
+import { saveJsonSettings } from '@/entities/User';
+import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
 
 interface ThemeSwitcherProps {
     className?: string;
@@ -11,12 +13,19 @@ interface ThemeSwitcherProps {
 
 export const ThemeSwitcher = memo(({ className }: ThemeSwitcherProps) => {
     const { toggleTheme } = useTheme();
+    const dispatch = useAppDispatch();
+
+    const onToggleHandler = useCallback(() => {
+        toggleTheme((newTheme) => {
+            dispatch(saveJsonSettings({ theme: newTheme }));
+        });
+    }, [dispatch, toggleTheme]);
 
     return (
         <Button
             theme={ThemeButton.CLEAR}
             className={classNames('', {}, [className])}
-            onClick={toggleTheme}
+            onClick={onToggleHandler}
         >
             <ThemeSwitcherIcon className={cls.icon} />
         </Button>
